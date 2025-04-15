85a2e074fe5e4fbb91ae3306a8e2e600130c52c4
sylviakay <realsylviakey@gmail.com>
2025-04-15 11:12:06 +0100

    first commit

---
+++ rental/src/script.js
@@ -0,0 +1,23 @@
+console.log(5 + 5);
+// values, datatypes, variables
+// values are basically a piece of data, its the fundamental unit of information in programming
+
+// DATA-TYPES
+// in javascript, every value has data types (you can have at least 6 different data types), Data types includes:
+// Numbers : so called floating point numbers ex:whole numbers with decimals (5.0, 6.0) rounding
+// string: sequence of characters A,B,C which are denoted with either single or double quote
+// Boolean: these are logical data types which can only be true or false
+// undefined: This is the value taken by a viariable that is empty or hasn't been assigned a value and will still be assigned a value in the future
+// - (variables are used to store values which you want to re-use)
+// - (variables can be declared using the let and const)
+// Null: when nothing is assigned to a variable which has a specific use case
+// Big integer (big int): used to get an accurate reading for larger numbers
+
+let variable1 = "3";
+console.log(variable1);
+
+let my;
+console.log(my);
+
+let my1 = null;
+console.log(my1);
